17152d5a72f8cb5a7ec4cf50224792e0586106d6
Victor Manuel Vilchis Nando <victorwd420@gmail.com>
2022-04-29 09:25:37 -0500

    Hoy se Realizara La Correcion de la navegacion de usuarios

--- Public/index.php
+++ Public/index.php
@@ -10,6 +10,7 @@ include (LAYOUT_PATH."head.php");
 ?>
 
     <body>
+        <h1> Hoy Se Realiazara la correcion de la navegacion de usuarios</h1>
 
     <?php  include (LAYOUT_PATH."header.php");?>
 
